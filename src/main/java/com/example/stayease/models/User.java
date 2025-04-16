0d45a0098712f19ba885d686e954f85a161c90b2
Erjon Zeqiri <erjonzeqiri200@gmail.com>
2025-04-16 15:10:08 +0200

    Creating the model and repository for users

---
+++ src/main/java/com/example/stayease/models/User.java
@@ -0,0 +1,33 @@
+package com.example.stayease.models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "fullname", nullable = false)
+    private String fullName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private int role;
+
+
+
+}
---
+++ src/main/java/com/example/stayease/repositories/UserRepository.java
@@ -0,0 +1,29 @@
+package com.example.stayease.repositories;
+import com.example.stayease.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query(value = "CALL sp_find_all_users()", nativeQuery = true)
+    List<User> findAllUsers();
+
+    @Query(value = "CALL sp_find_user_by_id(:id)", nativeQuery = true)
+    Optional<User> findUserById(@Param("id") Integer id);
+
+    @Query(value = "CALL sp_save_user(:#{#user.fullName}, :#{#user.email}, :#{#user.password}, :#{#user.role})",
+            nativeQuery = true)
+    User saveUser(@Param("user") User user);
+
+    @Query(value = "CALL sp_update_user(:#{#user.id}, :#{#user.fullName}, :#{#user.email}, :#{#user.password}, :#{#user.role})",
+            nativeQuery = true)
+    User updateUser(@Param("user") User user);
+
+    @Query(value = "CALL sp_delete_user(:id)", nativeQuery = true)
+    void deleteUserById(@Param("id") Integer id);
+}
